2b98dfefe4283b56a8c1394695406892b66ecd98
DotHide <dothide@gmail.com>
2013-12-01 12:40:25 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '28f218fb5f726aaf347bf0bacf679aa48f932dc82f18d2e78b790dae6cd58b7fa471e62549fdd11523be0cb4be4ace3b4411b27e91afe96208b655ff15737b28'
